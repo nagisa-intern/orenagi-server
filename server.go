68648f8279e9ecbf78d25f08aa0b9bd2350c1da7
k33asby <k33asby@gmail.com>
2018-09-01 18:35:08 +0900

    Add gorm

--- server.go
+++ server.go
@@ -3,12 +3,48 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/naoina/genmai"
 )
 
 const (
 	EXPOSED_PORT = ":8080"
 )
+
+type Authors struct {
+    gorm.Model
+		name string
+		info string
+}
+
+type Comics struct {
+    gorm.Model
+		title   string
+		summary string
+}
+
+type Location struct {
+    gorm.Model
+    Name string `gorm:"size:255"`
+    Password string `gorm:"size:255"`
+    Email string `gorm:"size:255"`
+}
+
+type ComicLocation struct {
+		gorm.Model
+		comic_id    int
+		location_id int
+}
+
+type ComicAuthor struct {
+		gorm.Model
+		comic_id  int
+		author_id int
+}
+
+type LocationsComic struct {
+		gorm.Model
+		comic_id    int
+		location_id int
+}
 
 func main() {
 	db, err := genmai.New(&genmai.MySQLDialect{}, "mysql:host=rds-intern.cc7ypgxhy74c.ap-northeast-1.rds.amazonaws.com;dbname=admin")
@@ -19,6 +55,11 @@ func main() {
 	router := gin.Default()
 	router.GET("/comic_list/get", func(c *gin.Context) {
 		locname := c.Query("locname")
+
+		var results []model.Comics
+		if err := db.Select(&results, db.Join(&model.)); err != nil {
+			panic(err)
+		}
 	})
 	router.Run(EXPOSED_PORT)
 
